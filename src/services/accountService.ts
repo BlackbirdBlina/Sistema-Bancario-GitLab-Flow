118023f20605f38dc2e9caa0a8544a1af80077b8
Sabrina Venceslau <sabrinapop_rn@hotmail.com>
2026-05-02 18:46:49 -0300

    feat(#2): cadastrar conta

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,3 +1,14 @@
-export function registerAccount() {
-  // TODO: Implement account registration
+import { accounts } from "@/store/accountStore";
+import { Account } from "@/interfaces/account";
+
+export function registerAccount(accountNumber: number) : Account{
+  if (accounts.has(accountNumber)) {
+    throw new Error('Número de conta ${accountNumber} já existe. Escolha outro número para a conta.');
+  }
+  const newAccount: Account = {
+    accountNumber,
+    balance: 0,
+  };
+  accounts.set(accountNumber, newAccount);
+  return newAccount;
 }
