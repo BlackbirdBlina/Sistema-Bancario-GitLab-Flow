2b33730b726162a3734c717f219bd34406afc0e6
Sabrina Venceslau <sabrinapop_rn@hotmail.com>
2026-05-02 19:06:25 -0300

    feat(#3): consultar saldo

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,3 +1,14 @@
+import { accounts } from "@/store/accountStore";
+import { Account } from "@/interfaces/account";
+
 export function registerAccount() {
   // TODO: Implement account registration
+}
+
+export function consultarSaldo(accountNumber: number) : number {
+  const account = accounts.get(accountNumber);
+  if (!account) {
+    throw new Error(`Conta ${accountNumber} não encontrada`);
+  }
+  return account.balance;
 }
