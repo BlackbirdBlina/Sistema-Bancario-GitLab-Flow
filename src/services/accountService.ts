b8b7db688d1811c9277d7c73c147a55d056637fd
Rodrigo Eduardo <rodrigoeduardodb1@gmail.com>
2026-05-03 21:52:03 -0300

    fix: indentação de código

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,9 +1,11 @@
 import { accounts } from "@/store/accountStore";
 import { Account } from "@/interfaces/account";
 
-export function registerAccount(accountNumber: number) : Account{
+export function registerAccount(accountNumber: number): Account {
   if (accounts.has(accountNumber)) {
-    throw new Error(`Número de conta ${accountNumber} já existe. Escolha outro número para a conta.`);
+    throw new Error(
+      `Número de conta ${accountNumber} já existe. Escolha outro número para a conta.`
+    );
   }
   const newAccount: Account = {
     accountNumber,
